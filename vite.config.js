208054792db1f42615243c5bfa0d00c9eb017c96
Benjamin Leong E-Jenn <e-jenn@hotmail.com>
2023-11-14 18:35:44 +0800

    Fix GitHub Pages Deployment Error

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "https://mydearben.github.io/lifewithben/",
+  build: {
+    outDir: "build",
+  },
 });
